323eb529a5b2f3d02fe201f69c7ae22864fa80ff
Rick Nieling <rick.nieling@nedap.com>
2024-06-12 17:27:58 +0200

    Error handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,18 +1,25 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 //Swagger
 {
     builder.Services.AddSwaggerGen();
 }
+
 var app = builder.Build();
 
 //Swagger
@@ -22,6 +29,8 @@ var app = builder.Build();
 }
 
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 }
